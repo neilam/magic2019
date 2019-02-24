d068c952182362f8fcd37361d32a8016a2b74a8c
Ruchi Mangtani <ruchimangtani@Ruchis-MacBook-Pro.local>
2019-02-23 17:08:52 -0800

    commit WhileLoops.java

---
+++ WhileLoops.java
@@ -0,0 +1,26 @@
+public class WhileLoops {
+public static void main(String[] args) {
+// while loops 
+
+	int x= 0;
+	while(x<5) {
+		System.out.println("Hello, x=" + x);
+		x++;
+		
+	}
+	
+
+	
+// print our numbers 1-10;
+	int number = 1; 
+	while(number<11) {
+		System.out.println(number);
+		number++;
+// number = number +1;
+	} 
+	for(int i=0; i<5; i++) {
+		System.out.println("Current i=" + i);
+
+}
+}
+}
